cb415a0bfbf070497f37e0acc2537dfa038f7e2e
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-07 16:51:05 +0300

    did the routes nad route

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React from 'react';
+
+import Home from './component/Home';
+import AddAerobicsExercise from './component/AddAerobicExercise';
+import Aerobics from './component/Aerobics';
+import About from './component/About';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './component/NavBar';
+
+
+
+function App() {
+ 
+
+
+
+  return (
+    <div>
+  <NavBar/>
+  <Routes>
+  <Route path='/' element = {<Home/>}/>
+  <Route path='/aerobics' element = {<Aerobics/>}/>
+  <Route path='/addaerobics' element = {<AddAerobicsExercise/>}/>
+  <Route path='/about' element = {<About/>}/>
+  
+
+</Routes>
+</div>
+      
+  );
+}
+
+export default App;
