aac38f06d7e4b40e307b92b8b81f27a6e6888308
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-07 17:41:03 +0300

    corrected errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import Home from './component/Home';
 import AddAerobicsExercise from './component/AddAerobicExercise';
 import Aerobics from './component/Aerobics';
 import About from './component/About';
-import { Routes, Route } from 'react-router-dom';
 import NavBar from './component/NavBar';
+
+
 
 
 
@@ -17,16 +18,16 @@ function App() {
 
   return (
     <div>
-  <NavBar/>
-  <Routes>
-  <Route path='/' element = {<Home/>}/>
-  <Route path='/aerobics' element = {<Aerobics/>}/>
-  <Route path='/addaerobics' element = {<AddAerobicsExercise/>}/>
-  <Route path='/about' element = {<About/>}/>
+    <NavBar/>
+    <Routes>
+    <Route path='/' element = {<Home/>}/>
+    <Route path='/aerobics' element = {<Aerobics/>}/>
+    <Route path='/addaerobics' element = {<AddAerobicsExercise/>}/>
+    <Route path='/about' element = {<About/>}/>
+    
   
-
-</Routes>
-</div>
+  </Routes>
+  </div>
       
   );
 }
