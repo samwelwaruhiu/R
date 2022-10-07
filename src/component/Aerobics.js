dc5e0a2e82bd4f88faec0cdf42d97b36e108ab53
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-07 16:56:56 +0300

    was able to fetch data from api

--- src/component/Aerobics.js
+++ src/component/Aerobics.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
-
+import Aerobiccard from "./Aerobiccard";
 
 
 function Aerobics(){
 
+
+    const [ex , setEx] = useState([])
+
+   useEffect(()=>
+   {
+    fetch("http://localhost:3000/exercises")
+    .then((resp) => resp.json())
+    .then((data) => setEx(data))
+   }, [])
+
+   
 
 
     return(
         <div>
             <h1>The Aerobic Page</h1>
             <div>
-                
+                {ex.map(exercises => <Aerobiccard key = {exercises.id} id= {exercises.id} ex={exercises} />)}
             </div>
            
 
