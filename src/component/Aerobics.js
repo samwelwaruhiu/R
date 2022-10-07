a31186aac8dec34eda62834325f7609efbd57c7d
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-07 16:55:19 +0300

    did the aerobic component

---
+++ src/component/Aerobics.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+
+
+function Aerobics(){
+
+
+
+    return(
+        <div>
+            <h1>The Aerobic Page</h1>
+            <div>
+                
+            </div>
+           
+
+        </div>
+    )
+}
+
+export default Aerobics;
