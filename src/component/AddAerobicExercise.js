f9e327ea48e6226d1a8ddaf236e290c86f94ee6a
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-07 17:10:23 +0300

    error correction in add... component

--- src/component/AddAerobicExercise.js
+++ src/component/AddAerobicExercise.js
@@ -3,68 +3,71 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';;
+import Stack from '@mui/material/Stack';
 
 function AddAerobicsExercise() {
 
+  const [newEx, setEx] = useState('');
+  const [newUrl, setnewUrl] = useState('');
+  const [newInstruction, setInstruction] = useState('');
+  const [newTime, setTime] = useState('');
 
-    const [newEx, setEx] = useState('');
-    const [newUrl, setnewUrl] = useState('');
-    const [newInstruction, setInstruction] = useState('');
-    const [newTime, setTime] = useState('');
-
-    function handlePost(e){
-        e.preventDefault();
-        fetch("http://localhost:3000/exercises",{
-            method : "POST",
-            headers : {"Content-Type" : "application/json"},
-            body:JSON.stringify({"title" : `${newEx}`, "pic_url" : `${newUrl}`,
-            "instructions": `${newInstruction}`,    "time" : `${newTime}`  
-          })
-    })}
-     
-
-    
-    return (
-        <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 12, width: '55ch' },
-        }}
-      >
+  function handlePost(e){
+    e.preventDefault();
+    fetch("http://localhost:3000/exercises",{
+        method : "POST",
+        headers : {"Content-Type" : "application/json"},
+        body:JSON.stringify({"title" : `${newEx}`, "pic_url" : `${newUrl}`,
+        "instructions": `${newInstruction}`,    "time" : `${newTime}`  
+      })
+})}
+ 
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 12, width: '55ch' },
+      }}
+    >
+      <TextField
+        id="outlined-name"
+        label="Name of Exercise"
+         onChange={(e) => setEx(e.target.value)}
+      />
+      <TextField
+        id="url"
+        label="Url of the Image"
+        onChange={(e) => setnewUrl(e.target.value)}
+      />
+       <TextField
+        id="description"
+        label="Instructions"
+        onChange={(e) => setInstruction(e.target.value)}
+      />
         <TextField
-          id="outlined-name"
-          label="Name of Exercise"
-           onChange={(e) => setEx(e.target.value)}
-        />
-        <TextField
-          id="url"
-          label="Url of the Image"
-          onChange={(e) => setnewUrl(e.target.value)}
-        />
-         <TextField
-          id="description"
-          label="Instructions"
-          onChange={(e) => setInstruction(e.target.value)}
-        />
-          <TextField
-          id="estimated_time"
-          label="Estimated time"
-          onChange={(e) => setTime(e.target.value)}
-          
-        />
+        id="estimated_time"
+        label="Estimated time"
+        onChange={(e) => setTime(e.target.value)}
         
-        <Stack direction="row" spacing={2}>
-      <Button variant="contained" onClick={handlePost}>
-          Send to Server
-        </Button>
-      </Stack>
-  
-      </Box>
-        
+      />
+      
+      <Stack direction="row" spacing={2}>
+    <Button variant="contained" onClick={handlePost}>
+        Send to Server
+      </Button>
+    </Stack>
+
+    </Box>
+
     
-        
-      );
-    }
+
     
-    export default AddAerobicsExercise;
+  );
+}
+
+export default AddAerobicsExercise;
+
+
+
+
+
