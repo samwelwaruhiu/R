70e5a27a4cca1fdccd7f8807d2a2b09c06a65eef
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-07 16:12:35 +0300

    did my home compnent

---
+++ src/component/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+
+function Home(){
+    return(
+        <section class = "header">
+        <div class = "text">
+           <h1>FITNESS WEBSITE</h1>
+           <p> Train with Us
+           </p>
+           
+        </div>
+    </section>
+         
+    )
+}
+
+ export default Home;
